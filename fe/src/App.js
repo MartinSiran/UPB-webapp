81628d8960dd013ef3425d68e207a180c7e1cb11
MartinSiran <matosiro4@gmail.com>
2022-10-20 16:29:51 +0200

    initial setup

---
+++ fe/src/App.js
@@ -0,0 +1,45 @@
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+
+const Abc = () => {
+  const [res, setRes] = useState("")
+
+  useEffect(() => {
+    axios.get(`http://localhost:5000/users`).then(res => {
+      const nieco = res.data;
+      setRes(nieco);
+    })
+  }, [])
+
+  return (
+    <div>
+      {res} - abc
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <Abc />
+    </div>
+  );
+}
+
+export default App;
