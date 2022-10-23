f231a428253808a154f0b762dc88f89173c610f8
MartinSiran <matosiro4@gmail.com>
2022-10-23 14:39:47 +0200

    remove unnecessary

--- api/app.js
+++ api/app.js
@@ -3,20 +3,6 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
-const mysql = require('mysql2');
-
-const con = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
-
-
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-});
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
