14d5ba4fdab2319ddc54fdd4f57e60e8c4bb7c52
Jakub Gura <gura.jakub@gmail.com>
2022-11-06 20:49:21 +0100

    change form to functional component

--- fe/src/components/Form.js
+++ fe/src/components/Form.js
@@ -3,92 +3,109 @@ import Button from 'react-bootstrap/Button';
 import {useState} from 'react';
 import { render } from 'react-dom';
 
-class Form extends React.Component {
+function Form() {
 
 
     // const [startDate, setStartDate] = useState('');
 
     // const [service, setService] = useState('');
 
-    constructor() {
-        super(); 
+    let displayData = [];
 
-        this.displayData = [];
+    const [val, setVal] = useState({
+        postVal : "",
+    });
 
-        this.state = {
-            showdata : this.displayData,
-            postVal : "",
-            startDate : "", 
-            service : "", 
-        }
+    const [data, setData] = useState({
+        showdata : displayData,
+    });
 
-        this.handleChangeDate = this.handleChangeDate.bind(this);
-        this.handleChangeService  = this.handleChangeService.bind(this);
-        this.createReservation = this.createReservation.bind(this)
-    }
+    const [date, setDate] = useState({
+        startDate : "",
+    });
 
-    handleChangeDate = event => {
-        this.setState({
+    const [serv, setServ] = useState({
+        service : ""
+    });
+
+    // let state = {
+    //     showdata : displayData,
+    //     postVal : "",
+    //     startDate : "", 
+    //     service : "", 
+    // }
+
+    // let handleChangeDate;
+    // let handleChangeService;
+
+    // handleChangeDate = handleChangeDate.bind(this);
+    // handleChangeService  = handleChangeService.bind(this);
+    // createReservation = createReservation.bind(this)
+
+    const handleChangeDate = event => {
+        setDate({
             startDate : event.target.value
         });
 
-        console.log('value is:', event.target.value);
+        console.log('value is:',  event.target.value);
     };
 
-    handleChangeService = event => {
-        this.setState({
+    const handleChangeService = event => {
+        setServ({
             service : event.target.value
         });
 
-        console.log('value is:', event.target.value);
+        console.log('value is:',  event.target.value);
     };
 
-    createReservation() {
-        console.log(this.state.startDate, '  ', this.state.service);
+    function createReservation() {
+        // console.log(date.startDate, '  ', serv.service);
 
-        this.displayData.push(<div  id="display-data mt-3"><p>{this.state.startDate}  {this.state.service}</p></div>); 
-        console.log(this.displayData);
-        this.setState({
-           showdata : this.displayData,
-           postVal : ""
+        displayData.push(<div  id="display-data mt-3"><p>{date.startDate}  {serv.service}</p></div>); 
+        console.log(displayData)
+        // console.log(displayData);
+        setData({
+           showdata : [data.showdata,displayData]
+        });
+
+        setVal({
+            postVal : ""
         });
     }
 
-    render(){
-        return (
-            <>
-                <h1 class="text-center">
-                Create reservation 
-                </h1>
-                <div class="d-flex justify-content-center mt-5">
-                    <div class="d-block">
-                        <div class="d-flex justify-content-start">
-                            <div class="flex">
-                                {/* <label for="startDate">Date */}
-                                <input id="startDate" class="form-control" type="date" onChange={this.handleChangeDate} value={this.state.startDate}/>
-                                {/* </label> */}
-                            </div>
-                            <div class="d-flex justify-content-end mx-1">
-                                <select class="form-select" aria-label="Default select example" onChange={this.handleChangeService}>
-                                    <option selected>Choose service</option>
-                                    <option value="Men's cut">Men's cut</option>
-                                    <option value="Trim the beard">Trim the beard</option>
-                                    <option value="Hair coloring">Hair coloring</option>
-                                </select>
-                            </div>
-                            <div class="d-flex justify-content-center mx-3">
-                                <Button onClick={this.createReservation} class="primary">Confirm</Button>
-                            </div>
+    return (
+        <>
+            <h1 class="text-center">
+            Create reservation 
+            </h1>
+            <div class="d-flex justify-content-center mt-5">
+                <div class="d-block">
+                    <div class="d-flex justify-content-start">
+                        <div class="flex">
+                            {/* <label for="startDate">Date */}
+                            <input id="startDate" class="form-control" type="date" onChange={handleChangeDate} value={date.startDate}/>
+                            {/* </label> */}
                         </div>
-                        <div id="display-data-Container">
-                            {this.displayData}
+                        <div class="d-flex justify-content-end mx-1">
+                            <select class="form-select" aria-label="Default select example" onChange={handleChangeService}>
+                                <option selected>Choose service</option>
+                                <option value="Men's cut">Men's cut</option>
+                                <option value="Trim the beard">Trim the beard</option>
+                                <option value="Hair coloring">Hair coloring</option>
+                            </select>
+                        </div>
+                        <div class="d-flex justify-content-center mx-3">
+                            <Button onClick={createReservation} class="primary">Confirm</Button>
                         </div>
                     </div>
+                    <div id="display-data-Container">
+                        {data.showdata}
+                    </div>
                 </div>
-            </>
-          )
-    }
-  
+            </div>
+        </>
+        
+    )
 }
 
 export default Form;
