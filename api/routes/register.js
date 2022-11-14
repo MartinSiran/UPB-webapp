cdb0d25bfd864017985b9e2fa86764d2fee67a0e
simonkrupa <simon.krupa@icloud.com>
2022-11-14 16:39:12 +0100

    fix register

--- api/routes/register.js
+++ api/routes/register.js
@@ -6,6 +6,7 @@ const registerUser = require('../authentication/registerUser')
 
 router.post('/', async function(req, res, next) {
   const isPasswordCommon = await registerUser.encryptPassword(req.body)
+  console.log(isPasswordCommon)
   res.send({ isPasswordCommon });
 });
 
