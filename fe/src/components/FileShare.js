1faf5d3752449eff6dda21011947400e6bbefacb
MartinSiran <matosiro4@gmail.com>
2022-11-28 01:40:35 +0100

    share files with users working

--- fe/src/components/FileShare.js
+++ fe/src/components/FileShare.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useState } from 'react'
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import AsyncSelect from 'react-select/async';
@@ -6,8 +6,11 @@ import axios from 'axios';
 
 function FileShare() {
 
-    const handleChange = value => {
-        console.log(value)
+    const [selectedUsers, setSelectedUsers] = useState([])
+
+    const handleChange = selectedVals => {
+        console.log(selectedVals)
+        setSelectedUsers(selectedVals.map(val => val.value))
     }
 
     const getUsers = () => {
@@ -16,16 +19,29 @@ function FileShare() {
         })
     }
 
+    const handleFileShare = (event) => {
+        event.preventDefault()
+        const formData = new FormData()
+        const file = event.target.fileToShare.files[0]
+        formData.append("file", file)
+        formData.append("users", JSON.stringify(selectedUsers))
+        console.log(formData)
+
+
+        axios.post(`${process.env.REACT_APP_API_HOST}/share`, formData)
+    }
+
     return (
         <>
             <h1 className="text-center">
                 Share file
             </h1>
+            <Form onSubmit={handleFileShare}>
             <div className="d-flex justify-content-center mt-5">
                 <div className="d-block">
                     <div className="d-flex justify-content-start">
                         <div className="flex">
-                            <Form.Group className="mb-3">
+                            <Form.Group className="mb-3" controlId='fileToShare'>
                                 <Form.Label>Upload file to share:</Form.Label>
                                 <Form.Control type="file" required />
                             </Form.Group>
@@ -39,15 +55,17 @@ function FileShare() {
                                     defaultOptions
                                     loadOptions={getUsers}
                                     onChange={handleChange}
+                                    required
                                 />
                             </Form.Group>
                         </div>
                         <div className="d-flex justify-content-center mx-3">
-                            <Button className="primary">Confirm</Button>
+                            <Button className="primary" type='submit'>Confirm</Button>
                         </div>
                     </div>
                 </div>
             </div>
+            </Form>
         </>
     )
 }
